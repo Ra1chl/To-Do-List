be384c5c6e085c034a54d857e501c9d38f1d5cc9
Brad Traversy <raichljonas1@gmail.com>
2025-05-22 10:28:40 +0200

    list-css

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -21,13 +21,20 @@ function List(){
     function handleFinishDateTask(event){
         setFinishDate(event.target.value);
     }
+    
+    function handleClearAll() {
+        const confirmClear = window.confirm("This will delete all tasks. Are you sure?");
+        
+        if (!confirmClear) return;
+        setTasks([]);
+  }
 
     return (<div className="tasks-container">
 
                 <h2 className="title">TO DO LIST</h2>
                 <ul className="tasks-list">
                     {tasks.map((tasks, index)=> 
-                        <li key={index}>
+                        <li key={index} className="task-item">
                             {tasks.taskName} | {tasks.taskFinishDate}
                         </li>
                     )}
@@ -36,7 +43,8 @@ function List(){
                 
                 <input className="input-field" type="text" value={name} onChange={handleNameTask} placeholder="Enter name of task"/><br/>
                 <input className="input-field" type="date" value={finishDate} onChange={handleFinishDateTask} placeholder="Enter date"/><br/>
-                <button onClick={handleTasks}>Add task</button>
+                <button className ="button" onClick={handleTasks}>Add task</button>
+                <button className ="delete-button" onClick={handleClearAll}>Delete all</button>
             </div>);
 }
 
