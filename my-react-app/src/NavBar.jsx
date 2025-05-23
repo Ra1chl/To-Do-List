0066086e083a6e5119f043bb09184e1f6d7a34cf
Brad Traversy <raichljonas1@gmail.com>
2025-05-23 14:49:45 +0200

    color picker

--- my-react-app/src/NavBar.jsx
+++ my-react-app/src/NavBar.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // nebo můžeš dát styl inline
+import "./Navbar.css";
 
 function Navbar() {
+  const [navbarColor, setNavbarColor] = useState("#0f172a");
+
+  const handleColorChange = (e) => {
+    setNavbarColor(e.target.value);
+  };
+
   return (
-    <nav className="navbar">
-      <h2 className="logo">TaskApp</h2>
+    <nav className="navbar" style={{ backgroundColor: navbarColor }}>
+      <div className="logo">TaskManager</div>
+
       <div className="nav-links">
-        <Link to="/">Sign Up</Link>
-        <Link to="/list">To-do List</Link>
+        <Link to="/">Register</Link>
+        <Link to="/list">Tasks</Link>
+
+        <input
+          type="color"
+          value={navbarColor}
+          onChange={handleColorChange}
+          className="color-picker"
+          title="Pick navbar color"
+        />
       </div>
     </nav>
   );
